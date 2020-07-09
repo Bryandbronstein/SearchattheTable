a4ea60f046a14f497fe6f455065dbc247689f1dc
bryandbronstein <bryandbronstein@gmail.com>
2020-07-09 12:26:57 -0400

    Updated npm to newest version

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function authorize(credentials, callback) {
     const {client_secret, client_id, redirect_uris} = credentials.installed;
     const oAuth2Client = new google.auth.OAuth2(
         client_id, client_secret, redirect_uris[0]);
-    testAuth = oAuth2Client;
+
     // Check if we have previously stored a token.
     fs.readFile(TOKEN_PATH, (err, token) => {
         if (err) return getAccessToken(oAuth2Client, callback);
