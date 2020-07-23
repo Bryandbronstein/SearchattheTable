eed163612ced7675a4b572751ee2914086374529
bryandbronstein <bryandbronstein@gmail.com>
2020-07-23 19:27:28 -0400

    Added episode titles as links Fixed issue where episode titles were counted in total results

---
+++ episode_links.php
@@ -0,0 +1,117 @@
+<?php
+$episodeLinks = array(
+    array(
+        "title" => "﻿Autumn in Hieron 00: We're Not Calling It Duckberg",
+        "link" => "https://drive.google.com/open?id=1xCBFsZqxr56ELVzXcpZ5nmH8bQO-rUHE66dbUEWgnA4"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 01: We Have Not Yet Begun To Be Pompous",
+        "link" => "https://drive.google.com/open?id=1sGQ3G9hATV_mBzrWLKPzFX2sZgdrGwIrleNBmUoC2O8"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 02: You Found Out What Was Magical",
+        "link" => "https://drive.google.com/open?id=1tKREOydDofdyNWsEiAYGa6Oe4rdgIXvbq57f-BQoJtk"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 03: A Podcast About Listening",
+        "link" => "https://drive.google.com/open?id=1_Lbd0qtfApJPfYVWj9e9mtn1yRUbOwrkyspZi-49MPM"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 04: Is It Time Already?",
+        "link" => "https://drive.google.com/open?id=1bQBl-pe7V3KXu_oY4FKJatFGEuKqjHzbzGBPkAewd2c"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 05: What’s a Good Name For A Ship?",
+        "link" => "https://drive.google.com/open?id=1XdbXmjv2injl8Ua7_KtpaBRzIvCsv23w269KvvV4rJs"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 06: A Bad Trip",
+        "link" => "https://drive.google.com/open?id=112IWBUU1Yes1A7u75xKSxvGjFtcktUzW0weW0uLPvN4"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 08: On The Tip Of Your Tongue",
+        "link" => "https://drive.google.com/open?id=1Qe-Za5Yq_60X2-xIINdzP3EAVXxicwAR4zzK991YhgQ"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 09: I’m Not Happy With This Decision At All",
+        "link" => "https://drive.google.com/open?id=1znte6qTjshc1vFiF7UE9H3tk1FwEcTjBsLuWWWkL0yU"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 10: Chekhov’s Torture Elf",
+        "link" => "https://drive.google.com/open?id=1B59UxpObG0U_ky8o5YbyRC4RC2xS-a9hbzJBB-DT_Kk"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 12: Knowledge and Ignorance",
+        "link" => "https://drive.google.com/open?id=1OSPBC5R-8M0KoAmJfjEQS86wqdxCWQWCSGRlLklbjQI"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 13: What A Surprise",
+        "link" => "https://drive.google.com/open?id=1IpnFHplPXGH2IOEpTs9g2w-yLGTEkpH2VQuawpUUK4o"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron: Holiday Special 02: I’ve Killed Monsters",
+        "link" => "https://drive.google.com/open?id=1B_GSuQ42l0Y2kcxiGl0gC1cgcKekDmV4DbmGsaQOV0w"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 15: Have You Ever Swung A Sword At A Ghost Before?",
+        "link" => "https://drive.google.com/open?id=1Zxf5R4YiPCyHkGneCSxnTA402GfJBfEM6I9_S3RFDD0"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 16: Those Woods You Chose",
+        "link" => "https://drive.google.com/open?id=1cnLCI-g5NAqt6Dd-IaHcNOraiKRujeZvOJY9GRqpKVI"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 17: As Death’s Eyes Look Over The World",
+        "link" => "https://drive.google.com/open?id=1vptygnyKDEPUnICnnpCTpYOpZxfbPebn-st23oZ2Yro"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 18: Magic (Or Something) Is All Around Us",
+        "link" => "https://docs.google.com/document/d/1WqnCE4BlWsiWBXR4z9SX6B7Q_StMaXbh6dAlPYPqdRg/edit?usp=sharing"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 19: Violence Mulligan Mulligan",
+        "link" => "https://drive.google.com/open?id=10fwGxLGO0KBnB9b5p0nyqvp2YB4csJB7JFNltMOoQj0"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 20: Do I Feel Followed?",
+        "link" => "https://drive.google.com/open?id=1uEEyEY0CcWKuo0pWSSXcRpbJUyO4haeiate3zYdeW4Y"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 21: The Storm Over Tristero",
+        "link" => "https://drive.google.com/open?id=1Q0-VP3_DdZzQ7xE9LmyK5kLi1Vqy6MTLOerdWtGwzxM"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 22: Closing the Window",
+        "link" => "https://drive.google.com/open?id=1F0BUHPkeX1ShpOWzxl_xr2BqbjlD4xsmHDwZLRQ_i1w"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 23: Spinning Straw Out Of Gold",
+        "link" => "https://drive.google.com/open?id=1xk-tRqgsn65kTy6gNplng6qYJKgNuGuLZ4BAP41c-1k"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 24: A Paladin is a Tool",
+        "link" => "https://drive.google.com/open?id=1ND8ah8CTIopM97F7UsMukMD_fMdtdEuBFFCM0rL6L4s"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 25: Work Off Your Sins",
+        "link" => "https://drive.google.com/open?id=1HFKCMM7f74dP30FNbzBq0ZkrvX2A7cLxYwLFmXNGeDE"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 26: Why Are You Here, Again?",
+        "link" => "https://drive.google.com/open?id=1TTy0x1iYdvYoeaolRe1a_yMqxUfO7PCt9yxC925p8TE"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 27: The Shores of Ordenna",
+        "link" => "https://drive.google.com/open?id=1N77h8Sd1wE5ZNjVxsBlE11AcqWGuaKsh_V0MxDfmqKk"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 28: A Choice About What You Believe",
+        "link" => "https://drive.google.com/open?id=1neSxYVTSu6T07ZtaGkoJXgBc8qCnd3XLThkuWopwehk"
+    ),
+    array(
+        "title" => "﻿Autumn in Hieron 29: Live Post Mortem",
+        "link" => "https://drive.google.com/open?id=1Nb73-BDfUzxYbNIDiVKGe6W-GvZNxFCfI7jHDNGtJbk"
+    )
+);
+$test1 = array_search("﻿Autumn in Hieron 27: The Shores of Ordenna", array_column($episodeLinks, 'title'));
+//var_dump($episodeLinks[$test1]['link']);
